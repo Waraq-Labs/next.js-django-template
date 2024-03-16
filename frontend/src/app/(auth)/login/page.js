42b490dc4d0bedaa8ce80a479bf3349cf64583ba
Asad Jibran Ahmed <me@asadjb.com>
2024-03-16 22:06:25 +0400

    style: update login form UI for better user experience

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -3,7 +3,7 @@
 import {BACKEND_URL} from '@/constants';
 import {useRouter} from 'next/navigation';
 import {useIsClient} from '@uidotdev/usehooks';
-import {Suspense, useState} from 'react';
+import {useState} from 'react';
 import {useAuthTokenData} from '@/lib/authentication';
 
 async function signIn(loginData, router, setToken) {
@@ -15,7 +15,7 @@ async function signIn(loginData, router, setToken) {
   const responseData = await response.json();
 
   if (!response.ok) {
-    return responseData
+    return responseData;
   }
 
   setToken(responseData);
@@ -29,14 +29,13 @@ function LoginForm() {
   const [error, setError] = useState(null);
 
   return <form
-      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
       onSubmit={async (event) => {
         event.preventDefault();
 
         setError(null);
 
         const signInResult = await signIn(
-            new FormData(event.target), router, setToken
+            new FormData(event.target), router, setToken,
         );
 
         if (signInResult) {
@@ -45,52 +44,58 @@ function LoginForm() {
           router.push('/');
         }
       }}>
+
+    <h2 className="text-2xl mb-6 text-center">Login</h2>
+
     <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2"
-             htmlFor="username">
-        Username
-      </label>
-      <input
-          name="username"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Username"/>
+      <label htmlFor="username" className="block font-light mb-1">Email
+        address</label>
+      <input id="username" name="username"
+             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:ring-1"/>
     </div>
-    <div className="mb-2">
-      <label className="block text-gray-700 text-sm font-bold mb-2"
-             htmlFor="password">
-        Password
-      </label>
-      <input
-          name="password"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password" type="password" placeholder="******************"/>
+
+    <div className="mb-4">
+      <label htmlFor="password"
+             className="block font-light mb-2">Password</label>
+      <input type="password" id="password" name="password"
+             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:ring-1"/>
     </div>
+
+    <div className="flex items-center mb-6">
+      <input type="checkbox" id="remember" className="mr-2"/>
+      <label htmlFor="remember" className="text-sm">Remember me</label>
+      <a href="#" className="text-sm text-blue-500 ml-auto">Forgot
+        password?</a>
+    </div>
+
     {error && <div className="mb-4">
-      <div id="error-message" className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+      <div id="error-message"
+           className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
         {error}
       </div>
     </div>}
-    <div className="flex items-center justify-between">
-      <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit">
-        Sign In
-      </button>
-      <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-         href="#">
-        Forgot Password?
-      </a>
-    </div>
+
+    <button
+        className="w-full bg-blue-500 text-white py-2 px-4 mb-2 rounded-md hover:bg-blue-600 transition duration-300">
+      Sign in
+    </button>
+    <button
+        className="w-full bg-gray-200 py-2 px-4 rounded-md hover:bg-gray-300 transition duration-300">
+      Create a new account
+    </button>
+
   </form>;
 }
 
 export default function LoginPage() {
   const isClient = useIsClient();
 
-  return <div className="flex justify-center items-center min-h-screen">
-    <div className="w-full max-w-md">
+  return <div
+      className="flex justify-center items-center min-h-screen bg-gray-50">
+    <div
+        className="w-full max-w-md bg-white border border-gray-300 shadow-md rounded-md p-8">
       {isClient && <LoginForm/>}
-      {!isClient && <Suspense fallback={<div>Loading...</div>}/>}
+      {!isClient && <div>Loading...</div>}
     </div>
   </div>;
 }
